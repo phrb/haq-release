021cdb998e4f1d59cf35f6457801010e0391766a
Pedro Bruel <pedro.bruel@gmail.com>
2020-04-19 14:13:47 -0300

    Fix syntax error

--- run_gpr.r
+++ run_gpr.r
@@ -246,7 +246,7 @@ for(i in 1:iterations){
         gpr_model <- km(formula = ~ .,
                         design = select(search_space, -Top5, -Top1),
                         response = y,
-                        nugget = = 1e-8 * var(y),
+                        nugget = 1e-8 * var(y),
                         control = list(pop.size = 400,
                                        BFGSburnin = 500))
 
