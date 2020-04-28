ae5c971976c036f5059e08f65ae30a6ae0f906cc
Pedro Bruel <pedro.bruel@gmail.com>
2020-04-28 15:40:54 -0300

    Add debug message

--- lib/env/quantize_env.py
+++ lib/env/quantize_env.py
@@ -226,6 +226,10 @@ class QuantizeEnv:
         min_bit, max_bit = self.bound_list[self.cur_ind]
         lbound, rbound = min_bit - 0.5, max_bit + 0.5  # same stride length for each bit
         action = (rbound - lbound) * action + lbound
+        print("Rounded values:")
+        print(str(int(np.round(action, 0))))
+        print("Trunc'ed values:")
+        print(str(int(np.trunc(action))))
         action = int(np.trunc(action))
         self.last_action = action
         return action  # not constrained here
